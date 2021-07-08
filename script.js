4b7f34fcc7ab59edad75d57db791ba930dc37aa8
ramadhankukuh <73922679+ramadhankukuh@users.noreply.github.com>
2021-07-08 08:31:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Adam";
+  sender = "Kukuh";
 }
 
 let footer = document.getElementById("credit");
 footer.innerHTML = sender;
-footer.href = "https://www.instagram.com/adamukti/";
+footer.href = "https://www.instagram.com/ramadhankukuh/";
 
 document.querySelector(".tombol").addEventListener('click', function () {
   Swal.fire("Hallo Sayangku", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
